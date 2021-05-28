331eae8fcab8ca29000c154a33cfc05cb73991fa
Fatih <fatihsertikin@outlook.com>
2021-05-28 16:43:32 +0200

    wip

--- Smart_City_Client/client.py
+++ Smart_City_Client/client.py
@@ -1,6 +1,10 @@
 from aiohttp import web
 import socketio
+import constants.commands as raspberryCommands
+import constants.response as raspberryResponses
+from sensorLibrary.Temperature import TemperatureSensor
 
+tempSensor = TemperatureSensor()
 
 ## creates a new Async Socket IO Server
 sio = socketio.AsyncServer(cors_allowed_origins='http://localhost:3000')
@@ -10,8 +14,7 @@ app = web.Application()
 ## instance
 sio.attach(app)
 
-## we can define aiohttp endpoints just as we normally
-## would with no change
+
 async def index(request):
     with open('index.html') as f:
         return web.Response(text=f.read(), content_type='text/html')
@@ -19,17 +22,15 @@ async def index(request):
 ## If we wanted to create a new websocket endpoint,
 ## use this decorator, passing in the name of the
 ## event we wish to listen out for
-@sio.on('message')
-async def print_message(sid, message):
-    ## When we receive a new event of type
-    ## 'message' through a socket.io connection
-    ## we print the socket ID and the message
-    print("Socket ID: " , sid)
-    print(message)
+@sio.on(raspberryCommands.GET_TEMPERATURE)
+async def getTemperature():
+    temp_in_celcius = tempSensor.read_temp()
+    print(f'read the temp: {temp_in_celcius}')
+    return temp_in_celcius
+
 
 ## We bind our aiohttp endpoint to our app
 ## router
-
 app.router.add_get('/', index)
 
 ## We kick off our server
