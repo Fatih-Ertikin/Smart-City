78451baa776a884f32b0b7ea247bb67485924404
Fatih <fatihsertikin@outlook.com>
2021-05-28 15:47:13 +0200

    wip

--- smart_city_webapp/src/websocket.js
+++ smart_city_webapp/src/websocket.js
@@ -1,7 +1,7 @@
 const io = require('socket.io-client');
 
-const ENDPOINT = 'http://localhost:4000';
-
+const ENDPOINT = 'http:/192.168.43.178:4000';
+// 192.168.43.178
 const connectionOptions = {
   'force new connection': true,
   reconnectionAttempts: 'infinity',
@@ -9,7 +9,7 @@ const connectionOptions = {
   transports: ['websocket'],
 };
 
-export const socket = io(ENDPOINT, connectionOptions);
+export const socket = io(ENDPOINT);
 
 socket.on('confirm_connection', () => {
   console.log('connection with server confirmed');
