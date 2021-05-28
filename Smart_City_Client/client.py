fd443ef08189e3a10ad019b3a47273185ba65030
Fatih <fatihsertikin@outlook.com>
2021-05-28 16:37:15 +0200

    wip

--- Smart_City_Client/client.py
+++ Smart_City_Client/client.py
@@ -1,8 +1,9 @@
 from aiohttp import web
 import socketio
 
+
 ## creates a new Async Socket IO Server
-sio = socketio.AsyncServer()
+sio = socketio.AsyncServer(cors_allowed_origins='http://localhost:3000')
 ## Creates a new Aiohttp Web Application
 app = web.Application()
 # Binds our Socket.IO server to our Web App
@@ -28,6 +29,7 @@ async def print_message(sid, message):
 
 ## We bind our aiohttp endpoint to our app
 ## router
+
 app.router.add_get('/', index)
 
 ## We kick off our server
