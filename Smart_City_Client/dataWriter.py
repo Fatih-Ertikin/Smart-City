84631e42f671bc898f1fd777383bab8001c44f7e
Fatih Ertikin <Fatih.s.ertikin@outlook.com>
2021-06-11 18:50:27 +0200

    s

--- Smart_City_Client/dataWriter.py
+++ Smart_City_Client/dataWriter.py
@@ -8,14 +8,16 @@ def writeData(temperature, soilMoisture):
     data = {'temperature': temperature, 'soilMoisture': soilMoisture}
     today = date.today()
     json_data = None
-    with open('data.json') as json_file:
-        json_data = json.load(json_file)
+
+    with open('data.json') as f:
+        json_data = json.load(f)
         print(f'[INFO] following JSON data was read: {json_data}')
-        json_file.close()
+        f.close()
     
     json_data[today] = data
     print(f'[INFO] writing new data: {json_data}')
-    print(json)
-    with open('data.json') as json_file:
+
+    with open('data.json') as f:
         json.dump(json_data, 'data.json')
+        f.close()
     print('[INFO] Succesfully wrote data')
